028bc2cb5a223eaa44ed81baeaa0ec3164309dcc
mieko nagatome <mieko_193@hotmail.com>
2015-08-05 11:27:01 -0500

    Initial commit

---
+++ app/src/main/java/com/team16/antenna/trend/drivingassistancesystem/BaseActivity.java
@@ -0,0 +1,7 @@
+package com.team16.antenna.trend.drivingassistancesystem;
+
+/**
+ * Created by Mieko on 05/08/2015.
+ */
+public class BaseActivity {
+}
---
+++ app/src/main/java/com/team16/antenna/trend/drivingassistancesystem/OBD.java
@@ -0,0 +1,14 @@
+package com.team16.antenna.trend.drivingassistancesystem;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class OBD extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_obd);
+    }
+}
